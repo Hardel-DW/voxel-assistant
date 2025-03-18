38b51ebc248d70eaea7465b2cb76bf90860666e8
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2025-03-18 22:52:18 +0100

    Initial Commit

---
+++ src/commands.js
@@ -0,0 +1,9 @@
+/**
+ * Share command metadata from a common spot to be used for both runtime
+ * and registration.
+ */
+
+export const FOO_COMMAND = {
+	name: "foo",
+	description: "Simple command that responds with Hello World",
+};
