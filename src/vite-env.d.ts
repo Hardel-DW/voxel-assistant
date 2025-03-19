b8490a269fb7efa97506c9f867af6e74fee6f6a2
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2025-03-19 10:17:10 +0100

    Remove virtual

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,1 @@
 /// <reference types="vite/client" />
-
-// Définition pour le module virtuel de réponses markdown
-declare module "virtual:markdown-responses" {
-    import type { MarkdownResponsesCache } from "./markdown-loader";
-    const responses: MarkdownResponsesCache;
-    export default responses;
-}
